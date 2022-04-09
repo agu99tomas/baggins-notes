0c6bc173d53d9050e6e74eb3a2038a3f10c2a61f
Tomás <agu99tomas@gmail.com>
2022-04-09 19:29:43 -0300

    refactor: code format

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -74,12 +74,8 @@ exports.verifyConfirm = [
       const query = { email: req.body.email };
       UserModel.findOne(query).then((user) => {
         if (!user) return res.unauthorized('Specified email not found.');
-        if (user.isConfirmed) {
-          return res.unauthorized('Account already confirmed.');
-        }
-        if (user.confirmOTP !== req.body.otp) {
-          return res.unauthorized('Otp does not match');
-        }
+        if (user.isConfirmed) return res.unauthorized('Account already confirmed.');
+        if (user.confirmOTP !== req.body.otp) return res.unauthorized('Otp does not match');
 
         UserModel.findOneAndUpdate(query, {
           isConfirmed: 1,
@@ -108,9 +104,7 @@ exports.resendConfirmOtp = [
       const query = { email: req.body.email };
       UserModel.findOne(query).then((user) => {
         if (!user) return res.unauthorized('Specified email not found.');
-        if (user.isConfirmed) {
-          return res.unauthorized('Account already confirmed.');
-        }
+        if (user.isConfirmed) return res.unauthorized('Account already confirmed.');
 
         const otp = otpGenerator.generate(4);
         const html = `<p>Please Confirm your Account.</p><p>OTP: ${otp}</p>`;
@@ -153,16 +147,8 @@ exports.login = [
 
         bcrypt.compare(req.body.password, user.password, (_err, equals) => {
           if (!equals) return res.unauthorized('Email or Password wrong.');
-          if (!user.isConfirmed) {
-            return res.unauthorized(
-              'Account is not confirmed. Please confirm your account.',
-            );
-          }
-          if (!user.active) {
-            return res.unauthorized(
-              'Account is not active. Please contact admin.',
-            );
-          }
+          if (!user.isConfirmed) return res.unauthorized('Account is not confirmed. Please confirm your account.');
+          if (!user.active) return res.unauthorized('Account is not active. Please contact admin.');
 
           const userData = {
             // eslint-disable-next-line no-underscore-dangle
