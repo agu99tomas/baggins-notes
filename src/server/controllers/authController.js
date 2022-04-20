17c2ef24a22bc132d2716f0f29281e4646b5b504
Tomás <agu99tomas@gmail.com>
2022-04-20 20:53:33 -0300

    refactor: remove try catch

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -18,44 +18,36 @@ const validator = require('../validators/authControllerValidator');
 exports.register = [
   validator.register,
   (req, res) => {
-    try {
-      encryptor.hash(req.body.password, (_err, hash) => {
-        const otp = otpGenerator.generate();
-        const html = `<p>Please Confirm your Account.</p><p>OTP: ${otp}</p>`;
-        mailer
-          .send(
-            req.body.email,
-            'Confirm Account',
-            html,
-          )
-          .then(() => {
-            const user = new UserModel({
-              firstName: req.body.firstName,
-              lastName: req.body.lastName,
-              email: req.body.email,
-              password: hash,
-              confirmOTP: otp,
-            });
+    encryptor.hash(req.body.password, (_err, hash) => {
+      const otp = otpGenerator.generate();
+      const html = `<p>Please Confirm your Account.</p><p>OTP: ${otp}</p>`;
+      mailer
+        .send(req.body.email, 'Confirm Account', html)
+        .then(() => {
+          const user = new UserModel({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hash,
+            confirmOTP: otp,
+          });
 
-            user.save((err) => {
-              if (err) return res.serverError(err);
+          user.save((err) => {
+            if (err) return res.serverError(err);
 
-              const userData = {
-                // eslint-disable-next-line no-underscore-dangle
-                id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-              };
+            const userData = {
+              // eslint-disable-next-line no-underscore-dangle
+              id: user._id,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.email,
+            };
 
-              return res.successWithData('Registration success', userData);
-            });
-          })
-          .catch(err => res.serverError(err));
-      });
-    } catch (err) {
-      return res.serverError(res, err);
-    }
+            return res.successWithData('Registration success', userData);
+          });
+        })
+        .catch(err => res.serverError(err));
+    });
   },
 ];
 
@@ -70,23 +62,19 @@ exports.register = [
 exports.verifyConfirm = [
   validator.verifyConfirm,
   (req, res) => {
-    try {
-      const query = { email: req.body.email };
-      UserModel.findOne(query).then((user) => {
-        if (!user) return res.unauthorized('Specified email not found.');
-        if (user.isConfirmed) return res.unauthorized('Account already confirmed.');
-        if (user.confirmOTP !== req.body.otp) return res.unauthorized('Otp does not match');
+    const query = { email: req.body.email };
+    UserModel.findOne(query).then((user) => {
+      if (!user) return res.unauthorized('Specified email not found.');
+      if (user.isConfirmed) return res.unauthorized('Account already confirmed.');
+      if (user.confirmOTP !== req.body.otp) return res.unauthorized('Otp does not match');
 
-        UserModel.findOneAndUpdate(query, {
-          isConfirmed: 1,
-          confirmOTP: null,
-        })
-          .then(() => res.success('Account confirmed success.'))
-          .catch(err => res.serverError(err));
-      });
-    } catch (err) {
-      return res.serverError(err);
-    }
+      UserModel.findOneAndUpdate(query, {
+        isConfirmed: 1,
+        confirmOTP: null,
+      })
+        .then(() => res.success('Account confirmed success.'))
+        .catch(err => res.serverError(err));
+    }).catch(err => res.serverError(err));
   },
 ];
 
@@ -100,20 +88,16 @@ exports.verifyConfirm = [
 exports.resendConfirmOtp = [
   validator.resendConfirmOtp,
   (req, res) => {
-    try {
-      const query = { email: req.body.email };
-      UserModel.findOne(query).then((user) => {
+    const query = { email: req.body.email };
+    UserModel.findOne(query)
+      .then((user) => {
         if (!user) return res.unauthorized('Specified email not found.');
         if (user.isConfirmed) return res.unauthorized('Account already confirmed.');
 
         const otp = otpGenerator.generate(4);
         const html = `<p>Please Confirm your Account.</p><p>OTP: ${otp}</p>`;
         mailer
-          .send(
-            req.body.email,
-            'Confirm Account',
-            html,
-          )
+          .send(req.body.email, 'Confirm Account', html)
           .then(() => {
             UserModel.findOneAndUpdate(query, {
               isConfirmed: 0,
@@ -121,11 +105,10 @@ exports.resendConfirmOtp = [
             })
               .then(() => res.success('Confirm otp sent.'))
               .catch(err => res.serverError(err));
-          });
-      });
-    } catch (err) {
-      return res.serverError(err);
-    }
+          })
+          .catch(err => res.serverError(err));
+      })
+      .catch(err => res.serverError(err));
   },
 ];
 
@@ -140,31 +123,27 @@ exports.resendConfirmOtp = [
 exports.login = [
   validator.login,
   (req, res) => {
-    try {
-      const query = { email: req.body.email };
-      UserModel.findOne(query).then((user) => {
-        if (!user) return res.unauthorized('Email or Password wrong.');
+    const query = { email: req.body.email };
+    UserModel.findOne(query).then((user) => {
+      if (!user) return res.unauthorized('Email or Password wrong.');
 
-        encryptor.compare(req.body.password, user.password, (_err, equals) => {
-          if (!equals) return res.unauthorized('Email or Password wrong.');
-          if (!user.isConfirmed) return res.unauthorized('Account is not confirmed. Please confirm your account.');
-          if (!user.active) return res.unauthorized('Account is not active. Please contact admin.');
+      encryptor.compare(req.body.password, user.password, (_err, equals) => {
+        if (!equals) return res.unauthorized('Email or Password wrong.');
+        if (!user.isConfirmed) return res.unauthorized('Account is not confirmed. Please confirm your account.');
+        if (!user.active) return res.unauthorized('Account is not active. Please contact admin.');
 
-          const userData = {
-            // eslint-disable-next-line no-underscore-dangle
-            id: user._id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-          };
+        const userData = {
+          // eslint-disable-next-line no-underscore-dangle
+          id: user._id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        };
 
-          userData.token = jwt.sign(userData);
+        userData.token = jwt.sign(userData);
 
-          res.successWithData('Login Success', userData);
-        });
+        res.successWithData('Login Success', userData);
       });
-    } catch (err) {
-      return res.serverError(res, err);
-    }
+    }).catch(err => res.serverError(err));
   },
 ];
